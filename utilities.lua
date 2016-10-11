d612bf7e099bf1f2e148b91435dcc21e6780b2c9
Mohammad Mahdi <MohammadArak@users.noreply.github.com>
2016-10-11 15:18:36 +0330

    Version 2.0

--- utilities.lua
+++ utilities.lua
@@ -1,3 +1,12 @@
+local logo = [[ 
+    _        _             _____
+   / \__   _(_)_ __ __ _  |_   _|__  __ _ _ __ ___
+  / _ \ \ / / | '__/ _` |   | |/ _ \/ _` | '_ ` _ \
+ / ___ \ V /| | | | (_| |   | |  __/ (_| | | | | | |
+/_/   \_\_/ |_|_|  \__,_|   |_|\___|\__,_|_| |_| |_|
+  
+]]
+
 function is_admin(msg)-- Check if user is admin or not
   local var = false
   for k,v in pairs(config.admin) do
