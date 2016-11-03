3fbbaca2a8b80d4e5460db01ee9fa49fee325708
Mohammad Mahdi <MohammadArak@users.noreply.github.com>
2016-11-03 11:16:16 +0330

    Remove additional functions

--- utilities.lua
+++ utilities.lua
@@ -17,20 +17,6 @@ function is_admin(msg)-- Check if user is admin or not
   return var
 end
 
-function write_file(path, text, mode)
-	if not mode then
-		mode = "w"
-	end
-	file = io.open(path, mode)
-	if not file then
-		return false --path uncorrect
-	else
-		file:write(text)
-		file:close()
-		return true
-	end
-end
-
 function clone_table(t) --doing "shit = table" in lua is create a pointer
   local new_t = {}
   local i, v = next(t, nil)
@@ -39,40 +25,4 @@ function clone_table(t) --doing "shit = table" in lua is create a pointer
     i, v = next(t, i)
   end
   return new_t
-end
-
-function download_to_file(url, file_path)
-  print("url to download: "..url)
-
-  local respbody = {}
-  local options = {
-    url = url,
-    sink = ltn12.sink.table(respbody),
-    redirect = true
-  }
-  -- nil, code, headers, status
-  local response = nil
-    options.redirect = false
-    response = {HTTPS.request(options)}
-  local code = response[2]
-  local headers = response[3]
-  local status = response[4]
-  if code ~= 200 then return false, code end
-
-  print("Saved to: "..file_path)
-
-  file = io.open(file_path, "w+")
-  file:write(table.concat(respbody))
-  file:close()
-  return file_path, code
-end
-
-function file_exists(name)
-  local f = io.open(name,"r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
 end
