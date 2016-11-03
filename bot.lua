7609aac3b7ad28b8a38cb4f1e00c77a50902b7c5
Mohammad Mahdi <MohammadArak@users.noreply.github.com>
2016-11-03 09:31:41 +0330

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -288,16 +288,17 @@ while is_running do -- Start a loop witch receive messages.
 	if response then
 		for i,msg in ipairs(response.result) do
 			last_update = msg.update_id
-			if msg.message or msg.inline_query then
+			if msg.message then
 				if msg.message.reply_to_message then
 					rethink_reply(msg.message)
 				elseif msg.message.forward_from then
 					forward_to_msg(msg.message)
-				elseif msg.inline_query then
-					inline_processor(msg.inline_query)
 				else
 					msg_processor(msg.message)
 				end
+			end
+			if msg.inline_query then
+				inline_processor(msg.inline_query)
 			end
 		end
 	else
